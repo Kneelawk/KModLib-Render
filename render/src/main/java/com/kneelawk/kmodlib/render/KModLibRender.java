746bd3a110e79a1e53bdbaa37d8a7e7b0bb6ac2b
Kneelawk <kneelawk@gmail.com>
2023-04-05 15:11:21 -0700

    Initial project setup

---
+++ render/src/main/java/com/kneelawk/kmodlib/render/KModLibRender.java
@@ -0,0 +1,10 @@
+package com.kneelawk.kmodlib.render;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KModLibRender implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
