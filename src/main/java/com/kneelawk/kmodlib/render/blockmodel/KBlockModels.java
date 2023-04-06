ea732b6322c88318df195f98bb0b4b47f303def0
Kneelawk <kneelawk@gmail.com>
2023-04-06 12:47:52 -0700

    Improve model parse error messages

--- src/main/java/com/kneelawk/kmodlib/render/blockmodel/KBlockModels.java
+++ src/main/java/com/kneelawk/kmodlib/render/blockmodel/KBlockModels.java
@@ -3,6 +3,7 @@ package com.kneelawk.kmodlib.render.blockmodel;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.google.gson.JsonObject;
 
@@ -27,6 +28,8 @@ import com.kneelawk.kmodlib.render.KMLRLog;
 import static com.kneelawk.kmodlib.render.Constants.id;
 
 public class KBlockModels {
+    public static final String MODEL_EXTENSION = ".kr.json";
+
     private static final Identifier BLOCK_MODEL_REGISTRY_ID = id("block_model");
     private static final Identifier BLOCK_MODEL_LAYER_REGISTRY_ID = id("block_model_layer");
     private static final RegistryKey<Registry<Codec<? extends KUnbakedModel>>> BLOCK_MODEL_REGISTRY_KEY =
@@ -37,6 +40,8 @@ public class KBlockModels {
         new SimpleRegistry<>(BLOCK_MODEL_REGISTRY_KEY, Lifecycle.stable());
     public static final Registry<Codec<? extends UnbakedModelLayer>> BLOCK_MODEL_LAYER_REGISTRY =
         new SimpleRegistry<>(BLOCK_MODEL_LAYER_REGISTRY_KEY, Lifecycle.stable());
+
+    private static final AtomicBoolean GAVE_FORMAT_WARNING = new AtomicBoolean(false);
 
     @SuppressWarnings("unchecked")
     public static void init() {
@@ -52,12 +57,20 @@ public class KBlockModels {
 
     private static ModelResourceProvider getResourceProvider(ResourceManager manager) {
         return (id, ctx) -> {
-            Identifier modelId = new Identifier(id.getNamespace(), "models/" + id.getPath() + ".kr.json");
+            Identifier modelId = new Identifier(id.getNamespace(), "models/" + id.getPath() + MODEL_EXTENSION);
             Optional<Resource> res = manager.getResource(modelId);
             if (res.isPresent()) {
                 try {
                     JsonObject object = JsonHelper.deserialize(new InputStreamReader(res.get().getInputStream()));
-                    return KUnbakedModel.CODEC.parse(JsonOps.INSTANCE, object).resultOrPartial(KMLRLog.LOG::warn)
+                    return KUnbakedModel.CODEC.parse(JsonOps.INSTANCE, object)
+                        .resultOrPartial(msg -> {
+                            KMLRLog.LOG.error("Error parsing k-render JSON model '{}': {}", modelId, msg);
+                            if (!GAVE_FORMAT_WARNING.getAndSet(true)) {
+                                KMLRLog.LOG.error(
+                                    "If you are generating custom JSON models, be aware that models ending in {} are picked up by KModLib-Render and interpreted as its custom model JSON format instead of Minecraft's default model JSON format.",
+                                    MODEL_EXTENSION);
+                            }
+                        })
                         .orElse(null);
                 } catch (IOException e) {
                     KMLRLog.LOG.warn("Error loading k-render model: {}", modelId, e);
